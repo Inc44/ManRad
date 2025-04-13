d47f9d2469dac3187df7a188d127d133d5284846
Inc <121856799+Inc44@users.noreply.github.com>
2025-04-13 19:02:03 +0200

    delete, save

--- _2a.py
+++ _2a.py
@@ -1,36 +1,63 @@
 from _0 import DIRS
-import os
 import json
+import os
+import sys
+
+DELETED_IMAGES_PATH = "deleted_images.json"
+IMAGES_PATH = "images.json"
+KEPT_IMAGES_PATH = "kept_images.json"
 
 
-def get_file_basenames(directory):
+def get_basenames(input_dir):
 	basenames = set()
-	if os.path.exists(directory):
-		for filename in os.listdir(directory):
-			if os.path.isfile(os.path.join(directory, filename)):
+	if os.path.exists(input_dir):
+		for filename in os.listdir(input_dir):
+			if os.path.isfile(os.path.join(input_dir, filename)):
 				basename = os.path.splitext(filename)[0]
 				basenames.add(basename)
 	return basenames
 
 
-def compare_directories():
-	image_dir = DIRS["image"]
-	image_resize_dir = DIRS["image_resized"]
-	original_files = get_file_basenames(image_dir)
-	resized_files = get_file_basenames(image_resize_dir)
-	kept_files = list(resized_files)
-	deleted_files = list(original_files - resized_files)
-	kept_files.sort()
-	deleted_files.sort()
-	output_dir = DIRS["merge"]
-	os.makedirs(output_dir, exist_ok=True)
-	kept_path = os.path.join(output_dir, "kept_files.json")
-	with open(kept_path, "w", encoding="utf-8") as f:
-		json.dump(kept_files, f, indent="\t")
-	deleted_path = os.path.join(output_dir, "deleted_files.json")
-	with open(deleted_path, "w", encoding="utf-8") as f:
-		json.dump(deleted_files, f, indent="\t")
+def get_filename(input_dir, basename):
+	if os.path.exists(input_dir):
+		for filename in os.listdir(input_dir):
+			if (
+				os.path.isfile(os.path.join(input_dir, filename))
+				and os.path.splitext(filename)[0] == basename
+			):
+				return filename
+	return None
 
 
 if __name__ == "__main__":
-	compare_directories()
+	if len(sys.argv) > 1:
+		mode = sys.argv[1]
+	merge_dir = DIRS["merge"]
+	images_dir = DIRS["image"]
+	resized_images_dir = DIRS["image_resized"]
+	deleted_images_path = os.path.join(merge_dir, DELETED_IMAGES_PATH)
+	images_path = os.path.join(merge_dir, IMAGES_PATH)
+	kept_images_path = os.path.join(merge_dir, KEPT_IMAGES_PATH)
+	if mode == "save":
+		images_paths = get_basenames(images_dir)
+		resized_images_paths = get_basenames(resized_images_dir)
+		kept_images = sorted(list(resized_images_paths))
+		deleted_images = sorted(list(images_paths - resized_images_paths))
+		images = sorted(list(images_paths))
+		with open(deleted_images_path, "w") as f:
+			json.dump(deleted_images, f, indent="\t")
+		with open(images_path, "w") as f:
+			json.dump(images, f, indent="\t")
+		with open(kept_images_path, "w") as f:
+			json.dump(kept_images, f, indent="\t")
+	else:
+		if not os.path.exists(deleted_images_path):
+			exit()
+		with open(deleted_images_path, "r") as f:
+			deleted_images = json.load(f)
+		for basename in deleted_images:
+			filename = get_filename(resized_images_dir, basename)
+			if filename:
+				path = os.path.join(resized_images_dir, filename)
+				if os.path.exists(path):
+					os.remove(path)
