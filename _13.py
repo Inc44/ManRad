37d940391ddd492cc4ae7557d3011b6684091594
Inc <121856799+Inc44@users.noreply.github.com>
2025-04-16 15:55:51 +0200

    bugfix

--- _13.py
+++ _13.py
@@ -3,13 +3,20 @@ import json
 import os
 
 if __name__ == "__main__":
-	with open(os.path.join(DIRS["merge"], "durations.json")) as f:
+	merge_dir = DIRS["merge"]
+	durations_path = os.path.join(merge_dir, "durations.json")
+	with open(durations_path) as f:
 		durations = json.load(f)
-	with open(os.path.join(DIRS["merge"], "gaps.json")) as f:
+	gaps_path = os.path.join(merge_dir, "gaps.json")
+	with open(gaps_path) as f:
 		gaps = json.load(f)
-	transition_gaps = gaps.copy()
-	for key in durations:
-		if key not in transition_gaps:
-			transition_gaps[key] = 0
-	with open(os.path.join(DIRS["merge"], "transition_gaps.json"), "w") as f:
+	duration_keys = sorted(durations.keys())
+	gaps_keys = sorted(gaps.keys())
+	transition_gaps = {}
+	for i in range(len(duration_keys)):
+		duration_key = duration_keys[i]
+		gap_key = gaps_keys[i]
+		transition_gaps[duration_key] = gaps[gap_key]
+	output_path = os.path.join(merge_dir, "transition_gaps.json")
+	with open(output_path, "w") as f:
 		json.dump(transition_gaps, f, indent="\t", ensure_ascii=False, sort_keys=True)
